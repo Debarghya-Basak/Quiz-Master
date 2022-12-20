0faf97518cc8397ca0fde223e9d2df578496c226
Debarghya-Basak <debarghyabasakofficial.2001@gmail.com>
2022-12-20 15:52:57 +0530

    Added soundFX

--- Assets/Scripts/QuizHandler.cs
+++ Assets/Scripts/QuizHandler.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class QuizHandler : MonoBehaviour
 {
+
     [SerializeField] GameObject questionField;
     [SerializeField] GameObject[] mcqButtons = new GameObject[4];
     [SerializeField] QuestionMaker[] questions = new QuestionMaker[100];
@@ -21,11 +22,14 @@ public class QuizHandler : MonoBehaviour
     float showAnswerTimer; 
     bool questionState = true;
 
+    [SerializeField] AudioClip[] sounds = new AudioClip[4];
+
     void Start()
     {
         //Initialize timer
         showQuestionTimer = defaultQuestionTimer;
-        showAnswerTimer = defaultAnswerTimer;    
+        showAnswerTimer = defaultAnswerTimer;   
+        GetComponent<AudioSource>().PlayOneShot(sounds[0]);
 
 
         questionText = questionField.GetComponent<TextMeshProUGUI>();
@@ -40,11 +44,17 @@ public class QuizHandler : MonoBehaviour
 
     void Update()
     {
+        timer();
+        
+    }
+
+    private void timer(){
         if(questionState){
             showQuestionTimer -= Time.deltaTime;
             Debug.Log("Question Timer : " + showQuestionTimer);
             if(showQuestionTimer <= 0){
                 //TODO: Show answer
+                GetComponent<AudioSource>().Stop();
 
                 showQuestionTimer = defaultQuestionTimer;
                 questionState = false;
@@ -55,6 +65,7 @@ public class QuizHandler : MonoBehaviour
             Debug.Log("Answer Timer : " + showAnswerTimer);
             if(showAnswerTimer <= 0){
                 //TODO: Show next question
+                GetComponent<AudioSource>().PlayOneShot(sounds[0]);
                 answerClicked = false;
                 enableOrDisableButtons(!answerClicked);
 
@@ -62,12 +73,12 @@ public class QuizHandler : MonoBehaviour
                 questionState = true;
             }
         }
-        
     }
 
     public void markAnswerGivenByUser(int index){
 
         Debug.Log(index + " button is clicked.");
+        GetComponent<AudioSource>().PlayOneShot(sounds[1]);
         answerClicked = true;
         enableOrDisableButtons(!answerClicked);
 
