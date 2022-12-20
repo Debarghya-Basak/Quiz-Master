ad0b53680a4e96729e59dca2320ff7f0ee9f1cef
Debarghya-Basak <debarghyabasakofficial.2001@gmail.com>
2022-12-20 21:55:06 +0530

    Added game logic

--- Assets/Scripts/QuizHandler.cs
+++ Assets/Scripts/QuizHandler.cs
@@ -3,26 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class QuizHandler : MonoBehaviour
 {
 
+    bool GAMESTATE = true;
+
+    // [Header("Questions")] 
     [SerializeField] GameObject questionField;
     [SerializeField] GameObject[] mcqButtons = new GameObject[4];
-    [SerializeField] QuestionMaker[] questions = new QuestionMaker[100];
+    [SerializeField] List<QuestionMaker> questions = new List<QuestionMaker>();
 
     TextMeshProUGUI questionText;
     TextMeshProUGUI[] mcqText = new TextMeshProUGUI[4];
 
     bool answerClicked = false;
 
-    float defaultQuestionTimer = 5f;
-    float defaultAnswerTimer = 2f;
+    [SerializeField] float defaultQuestionTimer = 5f;
+    [SerializeField] float defaultAnswerTimer = 2f;
     float showQuestionTimer;
     float showAnswerTimer; 
     bool questionState = true;
+    int questionNumberIndex = 0;
+    int globalClickedIndex = -1;
 
     [SerializeField] AudioClip[] sounds = new AudioClip[4];
+
+    [SerializeField] Sprite[] buttonColors = new Sprite[4];
 
     void Start()
     {
@@ -31,53 +39,91 @@ public class QuizHandler : MonoBehaviour
         showAnswerTimer = defaultAnswerTimer;   
         GetComponent<AudioSource>().PlayOneShot(sounds[0]);
 
-
-        questionText = questionField.GetComponent<TextMeshProUGUI>();
-        questionText.text = questions[0].getQuestion();
-
-        for(int i=0;i<4;i++){
-            mcqText[i] = mcqButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            mcqText[i].text = questions[0].getMCQAtIndex(i);
-        }
-        
+        displayQuestionAtIndex(0);
+       
     }
 
     void Update()
     {
         timer();
-        
+    }
+
+    private void displayQuestionAtIndex(int index){
+
+        questionText = questionField.GetComponent<TextMeshProUGUI>();
+        questionText.text = questions[index].getQuestion();
+
+        for(int i=0;i<4;i++){
+            mcqText[i] = mcqButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            mcqText[i].text = questions[index].getMCQAtIndex(i);
+        }
+
     }
 
     private void timer(){
-        if(questionState){
+
+        if(GAMESTATE){
+            if(questionState){
             showQuestionTimer -= Time.deltaTime;
-            Debug.Log("Question Timer : " + showQuestionTimer);
-            if(showQuestionTimer <= 0){
-                //TODO: Show answer
-                GetComponent<AudioSource>().Stop();
+                Debug.Log("Question Timer : " + showQuestionTimer);
+                if(showQuestionTimer <= 0){
+                    //TODO: Show answer
+                    GetComponent<AudioSource>().Stop();
+                    //Correct answer
+                    if(questions[questionNumberIndex].getCorrectAnswerIndex() == globalClickedIndex){
+                        mcqButtons[questions[questionNumberIndex].getCorrectAnswerIndex()].GetComponent<Image>().sprite = buttonColors[2];
+                        GetComponent<AudioSource>().PlayOneShot(sounds[2]);
+                    }//Wrong answer
+                    else if(questions[questionNumberIndex].getCorrectAnswerIndex() != globalClickedIndex && globalClickedIndex > -1){
+                        mcqButtons[questions[questionNumberIndex].getCorrectAnswerIndex()].GetComponent<Image>().sprite = buttonColors[2];
+                        mcqButtons[globalClickedIndex].GetComponent<Image>().sprite = buttonColors[3];
+                        GetComponent<AudioSource>().PlayOneShot(sounds[3]);
+                    }
+                    else{
+                        Debug.Log("Not clicked");
+                        mcqButtons[questions[questionNumberIndex].getCorrectAnswerIndex()].GetComponent<Image>().sprite = buttonColors[2];
+                        GetComponent<AudioSource>().PlayOneShot(sounds[3]);
+                    }
 
-                showQuestionTimer = defaultQuestionTimer;
-                questionState = false;
+                    showQuestionTimer = defaultQuestionTimer;
+                    questionState = false;
+                }    
             }
-        }
-        else{
-            showAnswerTimer -= Time.deltaTime;
-            Debug.Log("Answer Timer : " + showAnswerTimer);
-            if(showAnswerTimer <= 0){
-                //TODO: Show next question
-                GetComponent<AudioSource>().PlayOneShot(sounds[0]);
-                answerClicked = false;
-                enableOrDisableButtons(!answerClicked);
+            else{
+                showAnswerTimer -= Time.deltaTime;
+                Debug.Log("Answer Timer : " + showAnswerTimer);
+                if(showAnswerTimer <= 0){
+                    //TODO: Show next question
+                    GetComponent<AudioSource>().PlayOneShot(sounds[0]);
+                    answerClicked = false;
+                    enableOrDisableButtons(!answerClicked);
 
-                showAnswerTimer = defaultAnswerTimer;
-                questionState = true;
-            }
+                    if(questionNumberIndex >= questions.Count){
+                        GAMESTATE = false;
+                        //SceneManager.LoadScene(Actu)
+                        enableOrDisableButtons(false);
+                        GetComponent<AudioSource>().Stop();
+                    }
+                    else
+                        displayQuestionAtIndex(++questionNumberIndex);
+                    globalClickedIndex = -1;
+                    for(int j=0;j<4;j++){
+                         mcqButtons[j].GetComponent<Image>().sprite = buttonColors[0];
+                    }
+
+                    showAnswerTimer = defaultAnswerTimer;
+                    questionState = true;        
+                }            
+            }    
+
         }
     }
 
     public void markAnswerGivenByUser(int index){
 
         Debug.Log(index + " button is clicked.");
+        globalClickedIndex = index;
+        mcqButtons[index].GetComponent<Image>().sprite = buttonColors[1];
         GetComponent<AudioSource>().PlayOneShot(sounds[1]);
         answerClicked = true;
         enableOrDisableButtons(!answerClicked);
